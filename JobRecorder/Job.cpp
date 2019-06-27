d8bb31c58813d575dab078ed407432e687ee7e0e
Sachith Rukshan <sachithrukshanmail@gmail.com>
2019-06-27 05:35:58 +0530

    job class constructors done

--- JobRecorder/Job.cpp
+++ JobRecorder/Job.cpp
@@ -2,18 +2,48 @@
 
 Job::Job()
 {
+	id = ++currentMaxId;
 }
 
-Job::Job(int ownerNumber, string address, int telNum, string phoneModel, int phoneSerialNum, string recievedDate)
+Job::Job(
+	int ownerNumber,
+	string address,
+	int telNum,
+	string phoneModel,
+	int phoneSerialNum,
+	string recievedDate) : Job()
 {
+	this->ownerNumber = ownerNumber;
+	this->address = address;
+	this->telNum = telNum;
+	this->phoneModel = phoneModel;
+	this->phoneSerialNum = phoneSerialNum;
+	this->recievedDate = recievedDate;
 }
 
-Job::Job(int ownerNumber, string address, int telNum, string phoneModel, int phoneSerialNum, string recievedDate, string deliveredDate)
+Job::Job(
+	int ownerNumber,
+	string address,
+	int telNum,
+	string phoneModel,
+	int phoneSerialNum,
+	string recievedDate,
+	string deliveredDate) : Job(ownerNumber, address, telNum, phoneModel, phoneSerialNum, recievedDate)
 {
+	this->deliveredDate = deliveredDate;
 }
 
-Job::Job(int ownerNumber, string address, int telNum, string phoneModel, int phoneSerialNum, string recievedDate, string deliveredDate, double price)
+Job::Job(
+	int ownerNumber,
+	string address,
+	int telNum,
+	string phoneModel,
+	int phoneSerialNum,
+	string recievedDate,
+	string deliveredDate,
+	double price) : Job(ownerNumber, address, telNum, phoneModel, phoneSerialNum, recievedDate, deliveredDate)
 {
+	this->price = price;
 }
 
 int Job::getId()
--- JobRecorder/Job.h
+++ JobRecorder/Job.h
@@ -5,6 +5,7 @@ using namespace std;
 class Job
 {
 private:
+	static int currentMaxId = 0;
 	int id;
 	int ownerNumber;
 	string address;
@@ -23,6 +24,18 @@ public:
 	int getId();
 	string getAddress();
 	void setAddress(string address);
-
+	int getOwnerNumber();
+	void setOwnerNumber(int ownerNumber);
+	int getTelNum();
+	void setTelNum(int telNum);
+	string getPhoneModel();
+	void setPhoneModel(string phoneModel);
+	int getPhoneSerialNum();
+	void setPhoneSerialNum(int phoneSerialNum);
+	string getRecievedDate();
+	void setRecievedDate(string recievedDate);
+	string getDeliveredDate();
+	void setDeliveredDate(string deliveredDate);
+	double getPrice();
+	void setPrice(double price);
 };
-
